bcd39207d619d38b440906bc31066ad48a4f7cab
U-DESKTOP-9OIDHC8\MSI <huynhngocminh2511@gmail.com>
2018-01-16 21:54:56 +0700

    Add Exercise06

---
+++ Exercise06/Models/Call.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Exercise06.Models
+{
+    public class Call
+    {
+        public string Name { set; get; }
+
+        public DateTime Time { set; get; }
+    }
+}
