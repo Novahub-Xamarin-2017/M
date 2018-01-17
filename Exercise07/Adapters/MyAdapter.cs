5aed1ebb4406553138b424a062c86ff72d120363
U-DESKTOP-9OIDHC8\MSI <huynhngocminh2511@gmail.com>
2018-01-17 11:27:31 +0700

    Add Exercise07

---
+++ Exercise07/Adapters/MyAdapter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+using Android.Views;
+using Android.Widget;
+using Android.Support.V7.Widget;
+using Android.Graphics;
+using System.Collections.Generic;
+
+namespace Exercise07.Adapters
+{
+    class MyAdapter : RecyclerView.Adapter
+    {
+        private List<string> items;
+
+        public MyAdapter()
+        {
+        }
+
+        public MyAdapter(List<string> countrys)
+        {
+            items = countrys;
+        }
+
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            var id = Resource.Layout.CardView;
+            var itemView = LayoutInflater.From(parent.Context).
+                   Inflate(id, parent, false);
+
+            return new MyAdapterViewHolder(itemView);
+        }
+
+        public override void OnBindViewHolder(RecyclerView.ViewHolder viewHolder, int position)
+        {
+            ((MyAdapterViewHolder)viewHolder).SetViewHolder(items[position]);
+        }
+
+        public override int ItemCount => items.Count;
+    }
+
+    public class MyAdapterViewHolder : RecyclerView.ViewHolder
+    {
+        private TextView textViewCountry;
+
+        public MyAdapterViewHolder(View itemView) : base(itemView)
+        {
+            textViewCountry = itemView.FindViewById<TextView>(Resource.Id.tv_country);
+
+            itemView.Click += delegate 
+            {
+                Toast.MakeText(itemView.Context, textViewCountry.Text, ToastLength.Short).Show();
+            };
+        }
+
+        public void SetViewHolder(string country)
+        {
+            textViewCountry.Text = country;
+
+            if (country.Length == 1)
+            {
+                textViewCountry.SetTextColor(Color.Red);
+                textViewCountry.SetBackgroundColor(Color.Blue);
+            } else
+            {
+                textViewCountry.SetTextColor(Color.Black);
+                textViewCountry.SetBackgroundColor(Color.White);
+            }
+        }
+    }
+}
---
+++ Exercise07/MainActivity.cs
@@ -0,0 +1,54 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.Widget;
+using Exercise07.Adapters;
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise07
+{
+    [Activity(Label = "Exercise07", Theme = "@android:style/Theme.Material.Light.DarkActionBar", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        private MyAdapter adapter;
+
+        private List<string> countrys;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Main);
+
+            var recyclerView = FindViewById<RecyclerView>(Resource.Id.recyclerView1);
+
+            var layoutManager = new LinearLayoutManager(this);
+            recyclerView.SetLayoutManager(layoutManager);
+
+            adapter = new MyAdapter();
+            var input = Assets.Open("Countrys.json");
+
+            using (var streamReader = new StreamReader(input))
+            {
+                var content = streamReader.ReadToEnd();
+                countrys = JsonConvert.DeserializeObject<List<string>>(content);
+
+                adapter = new MyAdapter(countrys);
+            }
+
+            recyclerView.SetAdapter(adapter);
+
+            var searchView = FindViewById<SearchView>(Resource.Id.sv_country);
+
+            searchView.QueryTextChange += delegate
+            {
+                adapter = new MyAdapter(countrys.Where(x => x.Contains(searchView.Query)).ToList());
+                recyclerView.SetAdapter(adapter);
+            };
+        }
+    }
+}
+
